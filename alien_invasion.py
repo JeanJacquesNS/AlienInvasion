5d9b2868855e3e45b5572922a44ee588ef48a2ca
JeanJacquesNS <jeanjackns7@gmail.com>
2022-10-13 15:54:20 +0200

    Fazendo com que o jogo pare quando esgotado as naves disponiveis

--- alien_invasion.py
+++ alien_invasion.py
@@ -26,10 +26,11 @@ def run_game():
 	#Inicia o laço principal do jogo
 	while True:
 		gf.check_events(ai_settings,screen,ship,bullets)
-		ship.update()
-		bullets.update()
-		gf.update_bullets(ai_settings,screen,ship,aliens,bullets)
-		gf.update_aliens(ai_settings,stats,screen,ship,aliens,bullets)
-		gf.update_screen(ai_settings,screen,ship,aliens,bullets)  
+		if stats.game_active:
+			ship.update()
+			bullets.update()
+			gf.update_bullets(ai_settings,screen,ship,aliens,bullets)
+			gf.update_aliens(ai_settings,stats,screen,ship,aliens,bullets)
+			gf.update_screen(ai_settings,screen,ship,aliens,bullets)  
 
 run_game()
